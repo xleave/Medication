b0590f3c9df1f4fde2d4b16c373545192d2cf960
mh22512 <mh22512@essex.ac.uk>
2024-11-06 21:19:43 +0000

    Restored git back to state before maven broke everything

---
+++ Medication App/src/User.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class User {
+
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void checkIfUserExists() {    //Read users.csv to see if entry for user already exists.
+        System.out.println("Please enter your name.");
+        Scanner userInputScanner = new Scanner(System.in);
+
+        try {
+            File userFile = new File("Medication Reminder App/src/main/java/com/example/medicationreminder/util/users.csv");
+            Scanner userFileScanner = new Scanner(userFile);
+            while (userFileScanner.hasNextLine()) {
+                String line = userFileScanner.nextLine();
+                System.out.println(line);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("The user file could not be found! Program will now close.");
+            System.exit(2);
+        }
+    }
+
+
+    public void userCreate() {
+        System.out.println("Please enter your details to create your new account.");
+        Scanner userInput = new Scanner(System.in);
+        name = userInput.next();
+        password = userInput.next();
+
+        //Write new details to user file.
+
+        //Create a new medication file for the user.
+    }
+
+    public void userLogin() {
+        System.out.println("Please enter your login credentials to log into your account.");
+        Scanner userInput = new Scanner(System.in);
+        password = userInput.next();
+        System.out.println(password);
+    }
+}
+
