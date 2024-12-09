ca42406103735c6179640693c0b894f03598c351
sa22275 <sa22275@essex.ac.uk>
2024-12-09 20:26:32 +0000

    [Driver : sa22275] [Navigator : mh22512] Made the HelpGUI fully functional , added details to the FAQ's. Added colour coding. Did some finetuning for the main class as well

--- Medication App GUI/src/main/java/Main.java
+++ Medication App GUI/src/main/java/Main.java
@@ -1,12 +1,22 @@
 import gui.*;
 import services.User;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class    Main {
 
     public static void main(String[] args) {
 
         // Create Login GUI
         LoginGUI loginGUI = new LoginGUI();
+
+        loginGUI.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
 
         // Get user input from Login GUI
         String username = loginGUI.getNameFieldContents();
