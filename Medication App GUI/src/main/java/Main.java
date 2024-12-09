d648c4921da75ea3a290c975acd584527276f712
Xumin Lu <xl24773@essex.ac.uk>
2024-12-09 21:09:14 +0000

    [Driver:xl24773] [Navigator:qy24014] Repairing the Launcher

--- Medication App GUI/src/main/java/Main.java
+++ Medication App GUI/src/main/java/Main.java
@@ -1,33 +1,13 @@
-import gui.*;
+import gui.LoginGUI;
 import services.User;
 
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-public class    Main {
+public class Main {
 
     public static void main(String[] args) {
-
-        // Create Login GUI
-        LoginGUI loginGUI = new LoginGUI();
-
-        loginGUI.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
+        // Launch the Login GUI on the Event Dispatch Thread
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            LoginGUI loginGUI = new LoginGUI();
+            loginGUI.setVisible(true);
         });
-
-        // Get user input from Login GUI
-        String username = loginGUI.getNameFieldContents();
-        String password = loginGUI.getPasswordFieldContents();
-
-        // Create modules.User object and check if user exists
-        User genericUser = new User(username, password);
-        genericUser.checkIfUserExists();
-
-        // Launch Main GUI with the user information
-        MainGUI mainGUI = new MainGUI(genericUser);
-        mainGUI.displayMainGUI(); // Call the method to display the GUI
     }
 }
