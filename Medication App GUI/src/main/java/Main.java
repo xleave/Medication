6b6d768e6e52b997eba0c3527b8b360b9043f216
sa22275 <sa22275@essex.ac.uk>
2024-12-10 13:25:41 +0000

    Added a security measure to main to close all tabs if necessary

--- Medication App GUI/src/main/java/Main.java
+++ Medication App GUI/src/main/java/Main.java
@@ -1,12 +1,23 @@
 import gui.LoginGUI;
 import services.User;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class Main {
 
     public static void main(String[] args) {
+
+
         // Launch the Login GUI on the Event Dispatch Thread
         javax.swing.SwingUtilities.invokeLater(() -> {
             LoginGUI loginGUI = new LoginGUI();
+            loginGUI.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    System.exit(0);
+                }
+            });
             loginGUI.setVisible(true);
         });
     }
